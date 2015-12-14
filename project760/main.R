07e55a6f2ffa797c1cfdf1030f4cb689e56bac06
adithya.bhatp <adithya.bhatp2@gmail.com>
2015-12-13 18:33:54 -0600

    main.R update

--- project760/main.R
+++ project760/main.R
@@ -17,9 +17,41 @@ base_test <- census.test[1:38];
 # KNN
 # 42, 42, 40, 39 - big losers. 38, 37, 36 are oook
 
-knn_train <- na.omit(base_train)[3:38]
-knn_test <- na.omit(base_test)[3:38]
-knn_train_labels <- as.factor(na.omit(base_train)[[1]])
+knn_train <- na.omit(base_train)[,3:38]
+knn_test <- na.omit(base_test)[1:1000,3:38]
+knn_train_labels <- as.factor((na.omit(base_train)[,])[[1]])
+knn_test_labels <- as.factor((na.omit(base_test)[1:1000,])[[1]])
 
 library(class)
-knn_test_labels = knn(knn_train, knn_test, knn_train_labels, k=1)
+knn_pred <- knn(data.matrix(knn_train), data.matrix(knn_test), knn_train_labels, k=1)
+
+library(gmodels)
+CrossTable(x = knn_test_labels, y = knn_pred, prop.chisq=FALSE)
+
+# C4.5
+library(RWeka)
+c45_train <- na.omit(base_train)[,1:38]
+c45_train$instance.weight = 1
+c45_train_labels <- as.factor((na.omit(base_train)[1:100,])[[1]])
+#control = Weka_control(R = TRUE, M = 10)
+model1 <- J48(Label~., data=c45_train)
+#model2 <- J48(Label~., data=c45_train, control = Weka_control(R = TRUE, A = TRUE))
+model2 <- J48(Label~., data=c45_train, control = Weka_control(U = TRUE))
+
+
+c45_test <- na.omit(base_test)[1:150000,1:38]
+c45_test_labels <- as.factor((na.omit(base_test)[1:10,])[[1]])
+c45_test$instance.weight = 1
+
+c45_pred <- predict(model1, c45_test)
+table(c45_test[,1], c45_pred)
+
+
+
+c45_pred2 <- predict(model2, c45_test)
+table(c45_test[,1], c45_pred)
+
+library(gmodels)
+CrossTable(x = c45_test[,1], y = c45_pred, prop.chisq=FALSE)
+CrossTable(x = c45_test[,1], y = c45_pred2, prop.chisq=FALSE)
+
