7865e74f70d9f9ac6fbcf721b36d09d1fb5c12a2
Adithya Bhat <adithyabhatp2@github.com>
2017-02-14 20:28:18 -0600

    Learning and and or - sanity check

--- NeuralNetworks/src/Lab2.java
+++ NeuralNetworks/src/Lab2.java
@@ -2,6 +2,8 @@ import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 
+import java.util.Random;
+
 /**
  * Created by adbhat on 2/12/17.
  */
@@ -19,6 +21,11 @@ public class Lab2 {
 
         System.out.println("Done");
 
+        learnOr();
+
+        learnAnd();
+
+
 //        double[] u = {1, 2, 3, 4 };
 //        double[] v = {2, 4 };
 //        double[] w = {2, 3, 4 };
@@ -28,4 +35,70 @@ public class Lab2 {
 //        System.out.println(crossProd.toString());
 
     }
+
+    public static void learnOr() {
+
+        NeuralNetwork orNN = new NeuralNetwork(2, 1, 0.1);
+        orNN.intialize();
+
+        System.out.println("Before training");
+        orNN.printWeightArrays();
+
+        Random random = new Random();
+
+        for(int i=0;i<1000;i++) {
+            boolean a = random.nextBoolean();
+            boolean b = random.nextBoolean();
+
+            boolean op = a || b;
+
+            double input[] = new double[2];
+            input[0] = a==true?1.0:0.0;
+            input[1] = b==true?1.0:0.0;
+
+            double output[] = new double[1];
+            output[0] = op==true?1.0:0.0;
+
+            orNN.predictAndTrain(input, false, output);
+
+            if(i%100 == 0) {
+                System.out.println("Epoch: "+i);
+                orNN.printWeightArrays();
+            }
+        }
+    }
+
+    public static void learnAnd() {
+
+        NeuralNetwork orNN = new NeuralNetwork(2, 1, 0.1);
+        orNN.intialize();
+
+        System.out.println("Before training");
+        orNN.printWeightArrays();
+
+        Random random = new Random();
+
+        for(int i=0;i<100000;i++) {
+            boolean a = random.nextBoolean();
+            boolean b = random.nextBoolean();
+
+            boolean op = a && b;
+
+            double input[] = new double[2];
+            input[0] = a==true?1.0:0.0;
+            input[1] = b==true?1.0:0.0;
+
+            double output[] = new double[1];
+            output[0] = op==true?1.0:0.0;
+
+            orNN.predictAndTrain(input, false, output);
+
+            if(i%100 == 0) {
+                System.out.println("Epoch: "+i);
+                orNN.printWeightArrays();
+            }
+        }
+    }
+
+
 }
